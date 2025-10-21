a0ca04bc35cc58e5e16b387c7eed092baa3c5d2f
Eduardo Alexander Estrella Escobedo <lalo.eaee@gmail.com>
2025-10-21 10:34:08 -0600

    feat: agregar CRUD completo para modulo catalogo_usos_internet
    * Crear modelo con reglas de validación y relaciones
    * Implementar controller con operaciones CRUD completas
    * Añadir modelo de búsqueda para filtrado de registros
    * Crear vistas para index, create, update, view y _form
    * Incluir Pjax para funcionalidad AJAX en la vista index
    * Agregar breadcrumbs y soporte para traducciones
    * Implementar estructura base para relaciones con otros modelos

---
+++ backend/controllers/CatalogoUsosInternetController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace backend\controllers;
+
+use backend\models\CatalogoUsosInternet;
+use backend\models\search\CatalogoUsosInternetSearch;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * CatalogoUsosInternetController implements the CRUD actions for CatalogoUsosInternet model.
+ */
+class CatalogoUsosInternetController extends Controller
+{
+    /**
+     * @inheritDoc
+     */
+    public function behaviors()
+    {
+        return array_merge(
+            parent::behaviors(),
+            [
+                'verbs' => [
+                    'class' => VerbFilter::className(),
+                    'actions' => [
+                        'delete' => ['POST'],
+                    ],
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Lists all CatalogoUsosInternet models.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        $searchModel = new CatalogoUsosInternetSearch();
+        $dataProvider = $searchModel->search($this->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single CatalogoUsosInternet model.
+     * @param int $id ID
+     * @return string
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Creates a new CatalogoUsosInternet model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return string|\yii\web\Response
+     */
+    public function actionCreate()
+    {
+        $model = new CatalogoUsosInternet();
+
+        if ($this->request->isPost) {
+            if ($model->load($this->request->post()) && $model->save()) {
+                return $this->redirect(['view', 'id' => $model->id]);
+            }
+        } else {
+            $model->loadDefaultValues();
+        }
+
+        return $this->render('create', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Updates an existing CatalogoUsosInternet model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param int $id ID
+     * @return string|\yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($this->request->isPost && $model->load($this->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        }
+
+        return $this->render('update', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Deletes an existing CatalogoUsosInternet model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param int $id ID
+     * @return \yii\web\Response
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the CatalogoUsosInternet model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param int $id ID
+     * @return CatalogoUsosInternet the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = CatalogoUsosInternet::findOne(['id' => $id])) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException(Yii::t('app', 'The requested page does not exist.'));
+    }
+}
---
+++ backend/views/catalogo-usos-internet/_form.php
@@ -0,0 +1,23 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var backend\models\CatalogoUsosInternet $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="catalogo-usos-internet-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
