ba922835727881b0a69412a159a5e9f60b3a3e3a
Eduardo Alexander Estrella Escobedo <lalo.eaee@gmail.com>
2025-10-21 11:48:31 -0600

    feat: agregar CRUD completo para modulo alum_recreacion_tiempo
    * Crear modelo con reglas de validación y relaciones
    * Implementar controller con operaciones CRUD completas
    * Añadir modelo de búsqueda para filtrado de registros
    * Crear vistas para index, create, update, view y _form
    * Incluir Pjax para funcionalidad AJAX en la vista index
    * Agregar breadcrumbs y soporte para traducciones
    * Implementar estructura base para relaciones con otros modelos

---
+++ backend/views/alum-recreacion-tiempo/_search.php
@@ -0,0 +1,38 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var backend\models\search\AlumRecreacionTiempoSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="alum-recreacion-tiempo-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'alumnos_id') ?>
+
+    <?= $form->field($model, 'sabes_usar_internet') ?>
+
+    <?= $form->field($model, 'tienes_acceso_internet') ?>
+
+    <?= $form->field($model, 'catalogo_lugares_acceso_principal_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
