4069172aa1a3d3ce2a4444f4a526c0bb51196ff5
Johana Olivo <johana.yoe21@gmail.com>
2025-10-21 11:51:13 -0600

    feat: agregar CRUD completo para modulo frecuencia_veces_semana
    - Crear modelo con reglas de validación y relaciones
    - Implementar controller con operaciones CRUD completas
    - Añadir modelo de búsqueda para filtrado de registros
    - Crear vistas para index, create, update, view y _form
    - Incluir Pjax para funcionalidad AJAX en la vista index
    - Agregar breadcrumbs y soporte para traducciones
    - Implementar estructura base para relaciones con otros modelos

---
+++ backend/models/FrecuenciaVecesSemana.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "frecuencia_veces_semana".
+ *
+ * @property int $id
+ * @property string $nombre
+ *
+ * @property AlumHabitosConsumo[] $alumHabitosConsumos
+ * @property EjercicioFisico[] $ejercicioFisicos
+ */
+class FrecuenciaVecesSemana extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'frecuencia_veces_semana';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre'], 'required'],
+            [['nombre'], 'string', 'max' => 150],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+        ];
+    }
+
+    /**
+     * Gets query for [[AlumHabitosConsumos]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAlumHabitosConsumos()
+    {
+        return $this->hasMany(AlumHabitosConsumo::class, ['frecuencia_veces_semana_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[EjercicioFisicos]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getEjercicioFisicos()
+    {
+        return $this->hasMany(EjercicioFisico::class, ['frecuencia_veces_semana_id' => 'id']);
+    }
+}
