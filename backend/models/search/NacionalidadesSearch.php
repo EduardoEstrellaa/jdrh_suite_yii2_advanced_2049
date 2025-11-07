da0e90c27c8be5ee1b6b9f9ca873f3633ed7ca6d
Eduardo Alexander Estrella Escobedo <lalo.eaee@gmail.com>
2025-11-06 19:15:52 -0600

    refactor(common): mover modelo Nacionalidades a common y regenerar CRUDs con Gii
    - Se movió el modelo Nacionalidades desde backend/models a common/models
    - Se actualizaron namespaces y referencias en el CRUD
    - Se regeneraron controladores, vistas y modelo de búsqueda con Gii

--- backend/models/search/NacionalidadesSearch.php
+++ backend/models/search/NacionalidadesSearch.php
@@ -4,10 +4,10 @@ namespace backend\models\search;
 
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use backend\models\Nacionalidades;
+use common\models\Nacionalidades;
 
 /**
- * NacionalidadesSearch represents the model behind the search form of `backend\models\Nacionalidades`.
+ * NacionalidadesSearch represents the model behind the search form of `common\models\Nacionalidades`.
  */
 class NacionalidadesSearch extends Nacionalidades
 {
