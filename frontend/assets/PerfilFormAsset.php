f9278e31b4c2406fdd1a7751c2d89bb482486063
Eduardo Alexander Estrella Escobedo <lalo.eaee@gmail.com>
2025-11-18 13:55:41 -0600

    feat(frontend/perfilFormAssets): crear PerfilFormAsset para formulario de perfil

---
+++ frontend/assets/PerfilFormAsset.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+class PerfilFormAsset extends AssetBundle
+{
+    public $sourcePath = '@common/web';
+
+    public $css = [
+        'css/card-custom.css',
+    ];
+
+    public $depends = [
+        'yii\web\YiiAsset',
+        'frontend\assets\AppAsset',
+    ];
+
+    public function init()
+    {
+        parent::init();
+
+        $this->publishOptions = [
+            'forceCopy' => YII_DEBUG,
+        ];
+    }
+
+    
+}
