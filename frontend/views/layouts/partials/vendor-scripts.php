0420db6bcfe1b29cf82b4206d6bfebf47c5bf3e6
Eduardo Alexander Estrella Escobedo <lalo.eaee@gmail.com>
2025-11-17 20:52:25 -0600

    fix: eliminar '+' al final del script sweetalert2 en vendor-scripts del frontend

--- frontend/views/layouts/partials/vendor-scripts.php
+++ frontend/views/layouts/partials/vendor-scripts.php
@@ -14,4 +14,4 @@ use yii\helpers\Url;
 <script src="<?= Url::base(true) ?>/theme/js/pages/dashboard-projects.init.js"></script>
 <script src="<?= Url::base(true) ?>/theme/libs/jsvectormap/js/jsvectormap.min.js"></script>
 <script src="<?= Url::base(true) ?>/theme/js/plugins.js"></script>
-<script src="<?= Url::base(true) ?>/theme/libs/sweetalert2/sweetalert2.min.js"></script>+
+<script src="<?= Url::base(true) ?>/theme/libs/sweetalert2/sweetalert2.min.js"></script>
